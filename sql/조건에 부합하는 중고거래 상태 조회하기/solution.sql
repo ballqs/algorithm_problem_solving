4d742436525a8c73bb54a39b3489c7092eeec886
ballqs <46590521+ballqs@users.noreply.github.com>
2024-07-18 12:29:43 +0900

    조건에 부합하는 중고거래 상태 조회하기

---
+++ sql/조건에 부합하는 중고거래 상태 조회하기/solution.sql
@@ -0,0 +1,12 @@
+SELECT UGB.BOARD_ID , UGB.WRITER_ID , UGB.TITLE , UGB.PRICE ,
+        (
+        CASE
+            WHEN UGB.STATUS = "SALE" THEN "판매중"
+            WHEN UGB.STATUS = "RESERVED" THEN "예약중"
+            WHEN UGB.STATUS = "DONE" THEN "거래완료"
+            ELSE ""
+        END
+        ) AS STATUS
+FROM USED_GOODS_BOARD UGB
+WHERE UGB.CREATED_DATE = "2022-10-05"
+ORDER BY UGB.BOARD_ID DESC;
